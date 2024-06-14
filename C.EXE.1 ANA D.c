cc0fcda98eeb6e11af92d15d0a84ed18ca038858
AnaDomingas <anaaliiviafl246@gmail.com>
2024-06-14 17:06:34 -0300

    arquivos em c

---
+++ C.EXE.1 ANA D.c
@@ -0,0 +1,10 @@
+    #include<math.h>
+    #include<stdio.h>
+    int main()
+    float idade;
+    printf("Digite sua idade: ");
+    scanf("%f",&idade);
+    if(idade>-18);
+    printf("Você é maior de idade");
+    if else("Você é menor de idade");
+    return 0
